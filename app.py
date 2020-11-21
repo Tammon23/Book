6396401f6baced513852ad38cfee65bb5679c9db
Tammon23 <Tammon2000@gmail.com>
2020-11-20 19:46:53 -0500

    if already logged in going to the home default address goes to the home page instead of the login page did rough work for postings page functions and verification

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import tool
 import random as rm
 import mysql.connector
-
+import sys
 from flask import Flask, render_template, redirect, url_for, request, session, jsonify
 
 # DB connection
@@ -14,6 +14,9 @@ app = Flask(__name__)
 # Init redirect
 @app.route('/')
 def index():
+    if 'logged_in' in session and session['logged_in']:
+        return redirect((url_for(('home'))))
+
     return redirect(url_for('login'))
 
 
@@ -71,7 +74,7 @@ def search():
             b['price'] = "$" + str(rm.randrange(600)) + "." + str(rm.randrange(100)).zfill(2)
 
         cur.close()
-        return render_template('search.html', books=books)
+        return render_template('search.html', books=books, search_term=query)
 
     # GET method, display search and search options
     else:
@@ -134,7 +137,7 @@ def verifyLogin(user=None):
         session['logged_in'] = True
         # session['email'] = user["UEmail"]
         # session['user_type'] = "user"
-        session['user_dict'] = user # all information needed can be found via this dict
+        session['user_dict'] = user  # all information needed can be found via this dict
 
         return redirect(url_for('home'))
     else:
@@ -143,7 +146,65 @@ def verifyLogin(user=None):
 
 @app.route('/posting')
 def posting():
+    # cur = conn.cursor()
+    # cur.execute("select CourseID from courses")
+    # courseIDs = cur.fetchall()
+    # courseIDs = [tup[0] for tup in courseIDs]
+
     return render_template("posting.html")
+
+
+@app.route('/verifyPosting', methods=['POST'])
+def verifyPosting():
+    cur = conn.cursor(dictionary=True)
+
+    BISBN = request.form["BISBN"]
+    BTitle = request.form["BTitle"]
+    BAuthor = request.form["BAuthor"]
+    BCourse = request.form["BCourse"]
+    BPic = request.form["BPic"]
+    BNumber = request.form["BNumber"]
+
+    # Verifying that all the fields provided
+    if BISBN == "":
+        return jsonify({'error': 'Missing ISBN'})
+
+    elif BTitle == "":
+        return jsonify({'error': 'Missing Title'})
+
+    elif BAuthor == "":
+        return jsonify({'error': 'Missing Author'})
+
+    elif BCourse == "":
+        return jsonify({'error': 'Missing Course Code'})
+
+    elif BNumber == "":
+        return jsonify({'error': 'Missing Number of Books'})
+
+    # verifying the are posting more than 0 of a particular book
+    if BNumber < 1:
+        return jsonify({'error': 'Must have positive number of books'})
+
+    # If no picture link was provided, then use the default one
+    if BPic == "":
+        BPic = "static/img/1559138382833.jpg"
+
+    # verifying the course code is a supported one
+    try:
+        insertion_command = "INSERT INTO Books (BNumber, BTitle, BAuthor, BISBN, BCourse, BPic) VALUES (%s, %s, %s, " \
+                            "%s, %s, %s)"
+
+        cur.execute(insertion_command, [BNumber, BTitle, BAuthor, BISBN, BCourse, BPic])
+
+    except mysql.connector.errors.IntegrityError:
+        cur.close()
+        return jsonify({'error': 'BCourse not an accepted one'})
+
+    except:
+        cur.close()
+        return jsonify({'error': sys.exc_info()[0]})
+
+    cur.close()
 
 
 if __name__ == '__main__':
